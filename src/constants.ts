c5f97b2da8c00e92a5491ed21bfba141cc09a4f1
Adrian Hupka <hupka.adrian@gmail.com>
2021-06-21 01:13:22 +0200

    Aligned default roles with adjusted schema

--- src/constants.ts
+++ src/constants.ts
@@ -1,30 +1,35 @@
 import { RBACPermissionsProps, RBACRolesProps } from "./data";
 
+export const RBAC_RULESETS_STATESTORE_KEY = "rbac:rbac_rulesets"; 
+export const RBAC_ROLES_STATESTORE_KEY = "rbac:roles";
+export const RBAC_PERMISSIONS_STATESTORE_KEY = "rbac:permissions";
+
 export const DEFAULT_ROLES: RBACRolesProps = {
   roles: [
     {
       id: "1",
       name: "rbac_admin",
       description: "Role has permissions to change the RBAC ruleset.",
-      transactionId: "genesis"
+      transactionId: Buffer.from("genesis", "utf-8")
     },
     {
       id: "2",
       name: "role_membership_admin",
       description: "Role has permissions to assign and remove roles from accounts.",
-      transactionId: "genesis"
+      transactionId: Buffer.from("genesis", "utf-8")
     },
     {
       id: "3",
       name: "super_admin",
       description: "Role inherits permissions from the two roles rbac_admin and role_membership_admin.",
-      transactionId: "genesis",
+      transactionId: Buffer.from("genesis", "utf-8"),
       inheritance: [
         "1",
         "2"
       ]
     }
-  ]
+  ],
+  latest: 3
 }
 
 export const DEFAULT_PERMISSIONS: RBACPermissionsProps = {
@@ -33,49 +38,49 @@ export const DEFAULT_PERMISSIONS: RBACPermissionsProps = {
       roleId: "1",
       resourceName: "roles",
       operationName: "read",
-      transactionId: "genesis"
+      transactionId: Buffer.from("genesis", "utf-8")
     },
     {
       roleId: "1",
       resourceName: "roles",
       operationName: "create",
-      transactionId: "genesis"
+      transactionId: Buffer.from("genesis", "utf-8")
     },
     {
       roleId: "1",
       resourceName: "roles",
       operationName: "update",
-      transactionId: "genesis"
+      transactionId: Buffer.from("genesis", "utf-8")
     },
     {
       roleId: "1",
       resourceName: "roles",
       operationName: "delete",
-      transactionId: "genesis"
+      transactionId: Buffer.from("genesis", "utf-8")
     },
     {
       roleId: "2",
       resourceName: "role_membership",
       operationName: "read",
-      transactionId: "genesis"
+      transactionId: Buffer.from("genesis", "utf-8")
     },
     {
       roleId: "2",
       resourceName: "role_membership",
       operationName: "create",
-      transactionId: "genesis"
+      transactionId: Buffer.from("genesis", "utf-8")
     },
     {
       roleId: "2",
       resourceName: "role_membership",
       operationName: "delete",
-      transactionId: "genesis"
+      transactionId: Buffer.from("genesis", "utf-8")
     },
     {
       roleId: "2",
       resourceName: "roles",
       operationName: "read",
-      transactionId: "genesis"
+      transactionId: Buffer.from("genesis", "utf-8")
     }
   ]
 }
