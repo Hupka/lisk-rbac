11e0e2a89d07902c7fd648798f04be2aeda3c72d
Adrian Hupka <hupka.adrian@gmail.com>
2021-06-22 22:30:39 +0200

    added validations: only apply registered roles, remove duplicates, sort roles

--- src/assets/assign_role.ts
+++ src/assets/assign_role.ts
@@ -1,7 +1,7 @@
 import { BaseAsset, ApplyAssetContext, ValidateAssetContext } from 'lisk-sdk';
 
 import { AssignRoleAssetProps, assignRoleAssetPropsSchema, RBACAccountProps } from '../data'
-import { isHexString } from '../utils';
+import { isHexString, readRBACRolesObject } from '../utils';
 
 export class AssignRoleAsset extends BaseAsset<AssignRoleAssetProps> {
 	public name = 'assignrole';
@@ -24,8 +24,28 @@ export class AssignRoleAsset extends BaseAsset<AssignRoleAssetProps> {
 
 		const account = await stateStore.account.get<RBACAccountProps>(Buffer.from(asset.address, 'hex'));
 
-		account.rbac.roles = [...new Set([...account.rbac.roles, ...asset.roles])];
-		await stateStore.account.set(account.address, account);
+		const rolesList = await readRBACRolesObject(stateStore);
 
+		if (!rolesList) {
+			throw new Error("ERR: no roles list in database");
+		}
+
+		// Check which roles are included in rolesList
+		const verifyAvailabilityMap = asset.roles.map(assetElem => ({ id: assetElem, bool: (!!rolesList.roles.find(x => x.id === assetElem))}))
+		const missingRoles = verifyAvailabilityMap.filter(elem => !elem.bool);
+		
+		if (missingRoles.length) {
+			const rolesString = missingRoles.map(elem2 => elem2.id);
+			throw new Error(`The roles with id(s) [${rolesString.join()}] are unknown. None of the provided roles were assigned.`);
+		}
+
+		// Remove duplicated roles
+		const uniqueIdList = [...new Set([...account.rbac.roles.map(elem => elem.id), ...asset.roles])];
+
+		// Sort 
+		const sortedUniqueIdList = uniqueIdList.sort((a, b)=> parseInt(a, 10)-parseInt(b, 10));
+
+		account.rbac.roles = [...sortedUniqueIdList.map(elem => ({id: elem, name: (rolesList.roles.find(x => x.id === elem))?.name as string}))]
+		await stateStore.account.set(account.address, account);
 	}
 }
