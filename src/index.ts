c5fb18535c17dd841382eacb89c86d876790706f
Adrian Hupka <hupka.adrian@gmail.com>
2021-06-26 14:56:24 +0200

    Removed legacy assign role membership transaction

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import { RbacModule } from './rbac_module';
 import { RbacGetRulesetCommand } from './commands/getRuleset';
-import { AssignRoleAsset } from './assets/assign_role';
 
 export {
   RbacModule,
   RbacGetRulesetCommand,
-  AssignRoleAsset,
 }
