6be9542ffd80b48eca0c941d6fa5fbdc4f7f238c
Adrian Hupka <hupka.adrian@gmail.com>
2021-06-26 21:46:43 +0200

    Bugfixing: role_membership:assign / :remove

--- src/assets/role_membership/role_membership_remove.ts
+++ src/assets/role_membership/role_membership_remove.ts
@@ -1,28 +1,35 @@
-import { Account } from '@liskhq/lisk-chain';
 import { BaseAsset, ApplyAssetContext, ValidateAssetContext, codec } from 'lisk-sdk';
+import { Account } from '@liskhq/lisk-chain';
 
 import {
-  assignRoleMembershipAssetPropsSchema,
-  AssignRoleMembershipAssetProps,
-  RBACAccountProps,
-  RBACRoleRecord,
+  RemoveRoleMembershipAssetProps,
   RoleAccounts,
   RoleAccountsSchema,
+  removeRoleMembershipAssetPropsSchema,
+  RBACAccountProps,
+  RBACRoleRecord,
 } from '../../data';
 
-import { RBAC_PREFIX, RBAC_ROLE_ACCOUNTS_STATESTORE_KEY } from '../../constants';
-import { readRBACRolesObject } from '../../rbac_db';
+import {
+  readRBACRolesObject,
+} from '../../rbac_db';
+
+import {
+  RBAC_PREFIX,
+  RBAC_ROLE_ACCOUNTS_STATESTORE_KEY,
+} from '../../constants';
+
 import { isHexString } from '../../utils';
 
-export class AssignRoleMembershipAsset extends BaseAsset<AssignRoleMembershipAssetProps> {
-  public name = 'role_membership:create';
-  public id = 6;
-  public scopes = [{ resource: "role_membership", operation: "create" }];
+export class RemoveRoleMembershipAsset extends BaseAsset<RemoveRoleMembershipAssetProps> {
+  public name = 'role_membership:remove';
+  public id = 7;
+  public scopes = [{ resource: "role_membership", operation: "delete" }];
 
   // Define schema for asset
-  public schema = assignRoleMembershipAssetPropsSchema;
+  public schema = removeRoleMembershipAssetPropsSchema;
 
-  public validate({ asset }: ValidateAssetContext<AssignRoleMembershipAssetProps>): void {
+  public validate({ asset }: ValidateAssetContext<RemoveRoleMembershipAssetProps>): void {
     if (asset.roles === []) {
       throw new Error(`No role is included. Include at least one role for assignment.`);
     }
@@ -42,7 +49,7 @@ export class AssignRoleMembershipAsset extends BaseAsset<AssignRoleMembershipAss
     }
   }
 
-  public async apply({ asset, stateStore, reducerHandler, transaction }: ApplyAssetContext<AssignRoleMembershipAssetProps>): Promise<void> {
+  public async apply({ asset, stateStore, reducerHandler, transaction }: ApplyAssetContext<RemoveRoleMembershipAssetProps>): Promise<void> {
 
     // 1. Verify that sender has ALL necessary permission to perform transaction
     const hasPermission: boolean[] = [];
@@ -60,46 +67,39 @@ export class AssignRoleMembershipAsset extends BaseAsset<AssignRoleMembershipAss
 
     // 2. Do nothing when sender account does not have role with sufficient permission
     if (hasPermission.filter(elem => !elem).length) {
-      throw new Error(`Account '${transaction.senderAddress.toString('hex')}' does not have sufficient permissions to perform '${this.name}'.`);
+      throw new Error(`Account "${transaction.senderAddress.toString('hex')}" does not have sufficient permissions to perform '${this.name}'.`);
     }
 
-    // 2. Remove duplicate accounts and roles in transaction
+    // 2. Remove duplicate accounts and roles
     const assetAccounts: string[] = [...new Set(asset.accounts)]
     const assetRoles: string[] = [...new Set(asset.roles)]
 
-    // 3.1 Load all accounts which should get roles assigned
+    // 3.1 Load all accounts which should get roles memberships removed
     const accounts: Account<RBACAccountProps>[] = [];
     for (const account of assetAccounts) {
       accounts.push(await stateStore.account.get<RBACAccountProps>(Buffer.from(account, 'hex')));
     }
 
-    // 3.2 Load all roles to be assigned
+    // 3.2 Load all roles which should be removed from accounts
     const rolesList = await readRBACRolesObject(stateStore);
 
     if (!rolesList) {
       throw new Error("ERR: no roles list in database");
     }
 
-    // 4. Check if the roles scheduled for being assigned actually exist in the database
     const reducedRolesList: RBACRoleRecord[] = [];
     for (const roleId of assetRoles) {
       const role = rolesList.roles.find(elem => elem.id === roleId)
       if (!role) {
+        // Check if the roles scheduled for being assigned actually exist in the database
         throw new Error(`The role with id '${roleId}' is unknown. None of the provided roles were assigned.`);
       } else {
         reducedRolesList.push(role);
       }
     }
 
-    // 5. Check if the roles have lifecycle state set "active", if not abord
-    const inactiveRole = reducedRolesList.find(elem => elem.lifecycle === "inactive");
-    if (inactiveRole) {
-      throw new Error(`At least role with id [${inactiveRole.id}] had been removed. Removed roles can't be assigned.`);
-    }
-
-    // 6. Update "accounts-per-role" tables
+    // 4. Update "accounts-per-role" tables
     for (const role of reducedRolesList) {
-
       // Read current table of accounts per role 
       const roleAccountsBuffer = await stateStore.chain.get(`${RBAC_ROLE_ACCOUNTS_STATESTORE_KEY}:${role.id}`);
       if (!roleAccountsBuffer) {
@@ -107,25 +107,25 @@ export class AssignRoleMembershipAsset extends BaseAsset<AssignRoleMembershipAss
       }
       const roleAccounts = codec.decode<RoleAccounts>(RoleAccountsSchema, roleAccountsBuffer);
 
-      // Insert all new accounts 
+      // Remove all accounts from this transaction
       for (const account of accounts) {
-        if (!roleAccounts.accounts.find(elem => Buffer.compare(elem, account.address) === 0 )) {
-          roleAccounts.accounts.push(account.address);
+        const index = roleAccounts.accounts.findIndex(elem => Buffer.compare(elem, account.address) === 0)
+        if (index >= 0) {
+          roleAccounts.accounts.splice(index, 1);
         }
       }
 
       await stateStore.chain.set(`${RBAC_ROLE_ACCOUNTS_STATESTORE_KEY}:${role.id}`, codec.encode(RoleAccountsSchema, roleAccounts));
     }
 
-    // 7. Assign roles to users
+    // 5. Remove roles from users
     for (const account of accounts) {
-      for (const role of reducedRolesList) {
-        if (!account.rbac.roles.find(elem => elem.id === role.id)) {
-          account.rbac.roles.push({ id: role.id, name: role.name })
+      for (const role of assetRoles) {
+        const index = account.rbac.roles.findIndex(elem => elem.id === role);
+        if (index >= 0) {
+          account.rbac.roles.splice(index, 1)
         }
       }
-
-      account.rbac.roles.sort((a, b) => parseInt(a.id, 10) - parseInt(b.id, 10))
       await stateStore.account.set(account.address, account);
     }
   }
